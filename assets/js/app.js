642fa6fc696f84774cf3efc78e7f20ae2bc185d8
iPAGroove <ipagroove@gmail.com>
2025-10-13 23:56:20 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDFj9gOYU49Df6ohUR5CnbRv3qdY2i_OmU",
   authDomain: "ipa-panel.firebaseapp.com",
   projectId: "ipa-panel",
-  storageBucket: "ipa-panel.appspot.com",
+  storageBucket: "ipa-panel.firebasestorage.app",
   messagingSenderId: "239982196215",
   appId: "1:239982196215:web:9de387c51952da428daaf2"
 };
